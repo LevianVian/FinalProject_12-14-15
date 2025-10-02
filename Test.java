33ea243f6cf6d46fb43d461680226396138361a4
LevianVian <artalevi4@gmail.com>
2025-10-02 14:20:56 +0700

    Deleting add the first line

--- Test.java
+++ Test.java
@@ -1,6 +1,6 @@
 public class Test {
     public static void main(String[] args) {
-        System.out.println("Hi, This is first line add by Levi");
+        System.out.println("Hi, This is first line");
         System.out.println("Levi add this line");
         System.out.println("Hisyam add this line");
     }
